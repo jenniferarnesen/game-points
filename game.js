d36d826adf75730afc9d9c2c633fc2a91ade2024
Jennifer Jones Arnesen <jennifer@Jennifers-MacBook-Pro.local>
2016-01-27 22:18:56 +0100

    starting a simple prototype

---
+++ game.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function () {
+	console.log('game is ready');
+
+	$('.red').click(function () {
+  		$('.red-score').text('yo');
+	});
+
+	$('.green').click(function () {
+  		$('.green-score').text('yo');
+	});
+
+	$('.blue').click(function () {
+  		$('.blue-score').text('yo');
+	});
+});
