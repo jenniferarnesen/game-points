b73d28688df009ac0609c9f5ab15c71d0533a3a8
Jennifer Jones Arnesen <jennifer@Jennifers-MacBook-Pro.local>
2016-01-27 22:26:15 +0100

    increment every time a button is clicked

--- game.js
+++ game.js
@@ -1,15 +1,45 @@
-jQuery(document).ready(function () {
+var game = (function () {
+	var reds = 0,
+		greens = 0,
+		blues = 0,
+
+		incrementReds = function () {
+			reds += 1;
+			return reds;
+		},
+
+		incrementGreens = function () {
+			greens += 1;
+			return greens;
+		},
+
+		incrementBlues = function () {
+			blues += 1;
+			return blues;
+		};
+
+		return {
+			incrementReds: incrementReds,
+			incrementGreens: incrementGreens,
+			incrementBlues: incrementBlues
+		};
+}());
+
+$(document).ready(function () {
 	console.log('game is ready');
 
 	$('.red').click(function () {
-  		$('.red-score').text('yo');
+		var reds = game.incrementReds();
+  		$('.red-score').text(reds);
 	});
 
 	$('.green').click(function () {
-  		$('.green-score').text('yo');
+		var greens = game.incrementGreens();
+  		$('.green-score').text(greens);
 	});
 
 	$('.blue').click(function () {
-  		$('.blue-score').text('yo');
+		var blues = game.incrementBlues();
+  		$('.blue-score').text(blues);
 	});
 });
