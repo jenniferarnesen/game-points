3f0aa4ee4893107114390e5f786a860f4da3996d
Jennifer Jones Arnesen <jennifer@Jennifers-MacBook-Pro.local>
2016-01-27 22:55:41 +0100

    basic score calculations

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 var game = (function () {
-	var reds = 0,
+	var self = this,
+		reds = 0,
 		greens = 0,
 		blues = 0,
+
+		scoring = {
+			reds: 20,
+			greens: 30,
+			blues: 15
+		};
 
 		incrementReds = function () {
 			reds += 1;
@@ -16,30 +23,69 @@ var game = (function () {
 		incrementBlues = function () {
 			blues += 1;
 			return blues;
+		},
+
+		redScore = function () {
+			return reds * scoring.reds;
+		},
+
+		greenScore = function () {
+			return greens * scoring.greens;
+		},
+
+		blueScore = function () {
+			return blues * scoring.blues;
+		},
+
+		bonusPoints = function () {
+			return 0;
+		},
+
+		totalPoints = function () {
+			return redScore() + greenScore() + blueScore();
 		};
 
-		return {
-			incrementReds: incrementReds,
-			incrementGreens: incrementGreens,
-			incrementBlues: incrementBlues
-		};
+	return {
+		incrementReds: 		incrementReds,
+		incrementGreens: 	incrementGreens,
+		incrementBlues: 	incrementBlues,
+		redScore: 			redScore,
+		greenScore: 		greenScore,
+		blueScore: 			blueScore,
+
+		bonusPoints: bonusPoints,
+		totalPoints: totalPoints 
+	};
 }());
 
 $(document).ready(function () {
-	console.log('game is ready');
+
+	updateTotals = function () {
+		$('.total-points').text(game.totalPoints());
+		$('.bonus-points').text(game.bonusPoints());
+	}
 
 	$('.red').click(function () {
 		var reds = game.incrementReds();
-  		$('.red-score').text(reds);
+  		$('.red-quantity').text(reds);
+  		$('.red-score').text(game.redScore());
+
+  		updateTotals();
 	});
 
 	$('.green').click(function () {
 		var greens = game.incrementGreens();
-  		$('.green-score').text(greens);
+  		$('.green-quantity').text(greens);
+  		$('.green-score').text(game.greenScore());
+
+  		updateTotals();
 	});
 
 	$('.blue').click(function () {
 		var blues = game.incrementBlues();
-  		$('.blue-score').text(blues);
+  		$('.blue-quantity').text(blues);
+  		$('.blue-score').text(game.blueScore());
+
+  		updateTotals();
 	});
 });
