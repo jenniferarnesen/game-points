a9a9b0a5f1d5e6441a06845281f13631c5905e05
Jennifer Jones Arnesen <jjarnesen@gmail.com>
2016-01-31 16:22:03 +0100

    tabs to spaces, and add README

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 /*global game, colorizer*/
 
 $(document).ready(function () {
-	var items = game.init();
+    var items = game.init();
 
-	Object.keys(items).forEach(function (item) {
-		var buttonMarkup = 
-			'<button value="' + item + '" \
-    			class="game-button" \
+    Object.keys(items).forEach(function (item) {
+        var buttonMarkup = 
+            '<button value="' + item + '" \
+                class="game-button" \
                 style="background-color:' + colorizer.get() + ';"> \
-    			' + item + ' \
+                ' + item + ' \
             </button>';
-		$('#button-container').append(buttonMarkup);
-	});
+        $('#button-container').append(buttonMarkup);
+    });
 
-	Object.keys(items).forEach(function (item) {
-		var trMarkup =
-			'<tr> \
-				<td>' + item + '</td> \
-				<td class="' + item + '-quantity"></td> \
-				<td class="' + item + '-score"></td> \
-			</tr>';
-		$('#score-table tbody').append(trMarkup);
-	});
+    Object.keys(items).forEach(function (item) {
+        var trMarkup =
+            '<tr> \
+                <td>' + item + '</td> \
+                <td class="' + item + '-quantity"></td> \
+                <td class="' + item + '-score"></td> \
+            </tr>';
+        $('#score-table tbody').append(trMarkup);
+    });
 
-	updateTotals = function () {
-		$('#total-points').text(game.total());
-		$('#bonus-points').text(game.bonus());
-	}
+    updateTotals = function () {
+        $('#total-points').text(game.total());
+        $('#bonus-points').text(game.bonus());
+    }
 
-	clearBoard = function () {
-		$('[class$=quantity], [class$=score]').text('');
-  		updateTotals();
-	}
+    clearBoard = function () {
+        $('[class$=quantity], [class$=score]').text('');
+        updateTotals();
+    }
 
-	$('.game-button').click(function () {
-		var item = $(this).attr('value'),
-			quantity = game.increment(item);
+    $('.game-button').click(function () {
+        var item = $(this).attr('value'),
+            quantity = game.increment(item);
 
         $('.'+ item + '-quantity').text(quantity);
-  		$('.' + item + '-score').text(game.score(item));
+        $('.' + item + '-score').text(game.score(item));
 
-  		updateTotals();
-	});
+        updateTotals();
+    });
 
-	$('#new-game-button').click(function () {
-		game.reset();
-		clearBoard();
-	})
+    $('#new-game-button').click(function () {
+        game.reset();
+        clearBoard();
+    })
 });
